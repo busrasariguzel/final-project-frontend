b09c33eec4e1e1d815cab52e99acbfc263bfbc1f
busrasariguzel <busra.sariguzel@codeimmersives.com>
2020-04-23 10:10:30 -0400

    added more to eachCororna file to show more data

--- src/components/Corona Component/EachCoronaCase.js
+++ src/components/Corona Component/EachCoronaCase.js
@@ -13,8 +13,12 @@ render(){
                 <div key={this.props.idx}>
 
                     <p style={{color:'blue', textDecoration:'underline'}}>{this.props.item.country}</p>
-                    <p>Total : {this.props.item.cases.total}</p>
-                    <p>New : {this.props.item.cases.new}</p>
+                    <p>Total Cases: {this.props.item.cases.total}</p>
+                    <p>New Cases: {this.props.item.cases.new}</p>
+                    <p>Total Deaths: {this.props.item.deaths.total}</p>
+                    <p>New Deaths : {this.props.item.deaths.new}</p>
+                    <p>Recovered: {this.props.item.cases.recovered}</p>
+                    <p>Critical: {this.props.item.cases.critical}</p>
 
                 </div>
         </div>
